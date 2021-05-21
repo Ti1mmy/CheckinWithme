ac986d2c18d5caa33ee35e85138e9011bccf3387
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2021-05-21 14:55:11 -0400

    database
    Co-Authored-By: Eric <43616400+ericj22@users.noreply.github.com>
    Co-Authored-By: k9canine <83562449+k9canine@users.noreply.github.com>
    Co-Authored-By: YangXu-0 <boyangfu1991@gmail.com>

--- connect_database.py
+++ connect_database.py
@@ -1,6 +1,10 @@
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
+from cassandra.cluster import Cluster, ExecutionProfile, EXEC_PROFILE_DEFAULT
+from cassandra.policies import WhiteListRoundRobinPolicy, DowngradingConsistencyRetryPolicy
+from cassandra.query import tuple_factory
 import json
+from datetime import date
 
 with open('config.json', 'r') as config:
     conf = json.load(config)
@@ -10,10 +14,24 @@ cloud_config['secure_connect_bundle'] = conf['secure_connect_bundle']
 
 auth_provider = PlainTextAuthProvider(conf["CLIENT_ID"], conf["CLIENT_SECRET"])
 cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
-session = cluster.connect()
+session = cluster.connect('my_moods')
 
-row = session.execute("select release_version from system.local").one()
-if row:
-    print(row)
-else:
-    print("An error occurred.")
+# row = session.execute("select title from movies_and_tv")
+
+def update_mood(uuid: int, mood: float):
+    today = str(date.today())
+    session.execute(f"""
+                    CREATE TABLE IF NOT EXISTS user{uuid} (
+                        date text,
+                        mood decimal,
+                        PRIMARY KEY (date, mood)
+                    )
+                    """)
+    session.execute(f"INSERT INTO user{uuid} (date, mood) VALUES ('{today}', {mood})")
+
+update_mood(330159366999244800, 0.738)
+
+my_moods = session.execute('select (date, mood) from user330159366999244800')
+
+for thing in my_moods:
+    print(thing)
